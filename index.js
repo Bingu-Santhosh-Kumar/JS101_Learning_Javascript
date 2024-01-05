7aaa17cd871929f292a4f6c270dea20b597a37ab
Bingu-Santhosh-Kumar <santhoshkumarbingu98@gmail.com>
2024-01-05 22:50:52 +0530

    git 1st problem

---
+++ index.js
@@ -0,0 +1,27 @@
+// Q1- Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+let school= "Masai School";
+let newline= "Transformation in Education";
+
+console.log(school);
+console.log(newline);
+
+// Q2-
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+let name="Santhosh";
+
+console.log(name);
+
+let fatherName = "Mr. Raja Aro";
+
+console.log(fatherName);
+
+let motherName = "Mrs. Ammalu";
+
+console.log(motherName);
